cbd7bad94d0a4361efc7a120371363ca1b873b1a
Ayush Kumawat <138853573+Ayushxx007@users.noreply.github.com>
2026-01-27 18:32:57 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ const cookieParser = require("cookie-parser");
 
 const app=express();
 dotenv.config();
-const PORTX=process.env.PORT; //portconfig.........xx
+const PORTX=process.env.PORT; //portconfig......
 
 
 
